7b1bc2e94d281c8e105a85a6d508c4c063d5d777
Haoling Dong <haolingdong@microsoft.com>
2024-05-21 16:48:43 +0800

    Add the allowed enum values after merge summary and description

--- javagen/src/main/java/com/azure/autorest/util/MethodUtil.java
+++ javagen/src/main/java/com/azure/autorest/util/MethodUtil.java
@@ -99,15 +99,16 @@ public class MethodUtil {
             description = parameter.getLanguage().getDefault().getDescription();
         }
 
-        // add allowed enum values
-        if (isProtocolMethod && parameter.getProtocol().getHttp().getIn() != RequestParameterLocation.BODY) {
-            description = MethodUtil.appendAllowedEnumValuesForEnumType(parameter, description);
-        }
-
         String javadocDescription = SchemaUtil.mergeSummaryWithDescription(summary, description);
         if (CoreUtils.isNullOrEmpty(javadocDescription)) { // fallback to dummy description only when both summary and description are empty
             javadocDescription = "The " + name + " parameter";
         }
+
+        // add allowed enum values
+        if (isProtocolMethod && parameter.getProtocol().getHttp().getIn() != RequestParameterLocation.BODY) {
+            javadocDescription = MethodUtil.appendAllowedEnumValuesForEnumType(parameter, javadocDescription);
+        }
+
         return javadocDescription;
     }
 
